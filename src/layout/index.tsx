08d8d18dbc072a18b6156aa0073f5323a725023f
Norbert Kalácska <hello@kalacskanorbert.me>
2020-04-01 16:00:35 +0200

    Translated layout footer, index and left components to typescript

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -9,13 +9,13 @@ import {
   Typography,
   Grid,
   Paper
-}  from '@material-ui/core';
+} from '@material-ui/core';
 
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 
 //import Link as LinkMUI from '@material-ui/core/Link';
-import { Menu as MenuIcon} from '@material-ui/icons';
+import { Menu as MenuIcon } from '@material-ui/icons';
 
 import Footer from './footer';
 import LeftNav from './left';
@@ -24,98 +24,117 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: 'flex'
   },
   toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
+    paddingRight: 24 // keep right padding when drawer closed
   },
 
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
     transition: theme.transitions.create(['width', 'margin'], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   menuButtonHidden: {
-    display: 'none',
+    display: 'none'
   },
   title: {
-    flexGrow: 1,
+    flexGrow: 1
   },
   appBarSpacer: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     height: '100vh',
-    overflow: 'auto',
+    overflow: 'auto'
   },
   container: {
     paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
+    paddingBottom: theme.spacing(4)
   },
   paper: {
     padding: theme.spacing(2),
     display: 'flex',
     overflow: 'auto',
-    flexDirection: 'column',
+    flexDirection: 'column'
   },
   fixedHeight: {
-    height: 240,
-  },
+    height: 240
+  }
 }));
 
-export default props => {
-  const { children } = props;
+interface Props {
+  children: React.ReactNode | JSX.Element;
+}
+
+const Index = ({ children }: Props): JSX.Element => {
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
+  const handleDrawerOpen = (): void => {
     setOpen(true);
+  };
+
+  const handleClose = (): void => {
+    setOpen(false);
   };
 
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+      <AppBar
+        position="absolute"
+        className={clsx(classes.appBar, open && classes.appBarShift)}
+      >
         <Toolbar className={classes.toolbar}>
           <IconButton
             edge="start"
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+            className={clsx(
+              classes.menuButton,
+              open && classes.menuButtonHidden
+            )}
           >
             <MenuIcon />
           </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
             MUI-List
           </Typography>
         </Toolbar>
       </AppBar>
-      <LeftNav open={open} onClose={() => setOpen(false)}/>
+      <LeftNav open={open} onClose={handleClose} />
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
         <Container maxWidth="lg" className={classes.container}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                {children}
-              </Paper>
+              <Paper className={classes.paper}>{children}</Paper>
             </Grid>
           </Grid>
-          <Footer/>
+          <Footer />
         </Container>
       </main>
     </div>
   );
-}
+};
+
+export default Index;
---
+++ src/layout/left.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+import { list } from '../list';
+
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Drawer,
+  IconButton
+} from '@material-ui/core';
+
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+
+import { ChevronLeft as ChevronLeftIcon } from '@material-ui/icons';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  toolbar: {
+    paddingRight: 24 // keep right padding when drawer closed
+  },
+  toolbarIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '0 8px',
+    ...theme.mixins.toolbar
+  },
+
+  title: {
+    flexGrow: 1
+  },
+  drawerPaper: {
+    position: 'relative',
+    whiteSpace: 'nowrap',
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  drawerPaperClose: {
+    overflowX: 'hidden',
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    width: theme.spacing(7),
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9)
+    }
+  }
+}));
+
+interface ListUnitProps {
+  label: string;
+  path: string;
+}
+
+const ListUnit = (props: ListUnitProps): JSX.Element => {
+  const { label, path } = props;
+  return (
+    <Link to={path}>
+      <ListItem button>
+        <ListItemText primary={label} />
+      </ListItem>
+    </Link>
+  );
+};
+
+const mainListItems = list.map((l, i) => (
+  <ListUnit key={i} path={l.path} label={l.label} />
+));
+
+interface LeftNavProps {
+  onClose: () => void;
+  open: boolean;
+}
+
+const LeftNav = (props: LeftNavProps): JSX.Element => {
+  const { onClose, open } = props;
+  const classes = useStyles();
+
+  const handleDrawerClose = (): void => {
+    onClose();
+  };
+
+  return (
+    <Drawer
+      variant="permanent"
+      classes={{
+        paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
+      }}
+      open={open}
+    >
+      <div className={classes.toolbarIcon}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
+      <Divider />
+      <List>
+        <ListUnit label={'Home'} path={'/'} />
+        {mainListItems}
+      </List>
+    </Drawer>
+  );
+};
+
+export default LeftNav;
