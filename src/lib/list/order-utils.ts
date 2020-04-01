e33f0ca3a7a11bf40f2c59c83718bc8318e1c90b
Norbert Kalácska <hello@kalacskanorbert.me>
2020-04-01 20:47:45 +0200

    Translated lib/list/order-utils to typescript

--- src/lib/list/order-utils.ts
+++ src/lib/list/order-utils.ts
@@ -1,7 +1,7 @@
 import NexysUtil from '@nexys/utils';
 const { get } = NexysUtil.ds;
 
-export const getAttribute = (attribute, a) => {
+export const getAttribute = (attribute: any, a: any): any => {
   const ac = get(attribute, a);
 
   if (typeof ac === 'string') {
@@ -9,26 +9,30 @@ export const getAttribute = (attribute, a) => {
   }
 
   return ac;
-}
+};
 
-export const order = (data, sortAttribute, sortDescAsc) => {
+export const order = (
+  data: any[],
+  sortAttribute: string,
+  sortDescAsc: boolean
+): any[] => {
   if (!sortAttribute) {
     return data;
   }
 
   // use function in utils
-  const compare = ( a, b, attribute ) => {
+  const compare = (a: any, b: any, attribute: string): number => {
     const ac = getAttribute(attribute, a);
     const bc = getAttribute(attribute, b);
 
-    if ( ac < bc ){
+    if (ac < bc) {
       return -1;
     }
-    if ( ac > bc ){
+    if (ac > bc) {
       return 1;
     }
     return 0;
-  }
+  };
 
   const ordered = data.sort((a, b) => compare(a, b, sortAttribute));
 
@@ -37,17 +41,24 @@ export const order = (data, sortAttribute, sortDescAsc) => {
   }
 
   return ordered;
-}
+};
 
-export const paginationBoundaries = (idx, nPerPage) => {
+export const paginationBoundaries = (
+  idx: number,
+  nPerPage: number
+): { start: number; end: number } => {
   const start = (idx - 1) * nPerPage;
-  const end = (idx) * nPerPage;
+  const end = idx * nPerPage;
 
   return { start, end };
-}
+};
 
-export const orderWithPagination = (data, idx, nPerPage) => {
+export const orderWithPagination = (
+  data: any[],
+  idx: number,
+  nPerPage: number
+): any[] => {
   const { start, end } = paginationBoundaries(idx, nPerPage);
 
   return data.slice(start, end);
-}
+};
