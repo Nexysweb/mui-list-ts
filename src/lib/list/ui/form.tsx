691fe54959b1237cba3b33fd50ccd3b3dc895844
Norbert Kalácska <hello@kalacskanorbert.me>
2020-04-01 19:47:18 +0200

    Translated lib/list/ui/form to typescript

---
+++ src/lib/list/ui/form.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface InputValue {
+  name: string;
+  value: string;
+}
+
+interface Props {
+  name: string;
+  onChange: (inputValue: InputValue) => void;
+  value: string;
+}
+
+export const SearchUnit = (props: Props): JSX.Element => {
+  const [value, setValue] = React.useState(props.value || '');
+  const { name, onChange } = props;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value;
+    const v: InputValue = { name, value };
+    setValue(value);
+    onChange(v);
+  };
+
+  return (
+    <input value={value} type="text" name={name} onChange={handleChange} />
+  );
+};
