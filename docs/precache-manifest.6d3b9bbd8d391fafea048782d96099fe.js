2eb03b1cffae77077e4c99223e16f4e1fdc89bb1
Johan <johan@macair2020>
2020-04-08 12:13:55 +0200

    new UI

---
+++ docs/precache-manifest.6d3b9bbd8d391fafea048782d96099fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072f8929ebaed0aee3367f77c5046e1f",
+    "url": "/mui-list-ts/index.html"
+  },
+  {
+    "revision": "360ca9045ae2bd14abfb",
+    "url": "/mui-list-ts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dc3a78696321245cac00",
+    "url": "/mui-list-ts/static/js/2.7f779e88.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mui-list-ts/static/js/2.7f779e88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "360ca9045ae2bd14abfb",
+    "url": "/mui-list-ts/static/js/main.2821e53a.chunk.js"
+  },
+  {
+    "revision": "136d511bdd7e3911514d",
+    "url": "/mui-list-ts/static/js/runtime-main.e1794409.js"
+  }
+]);
