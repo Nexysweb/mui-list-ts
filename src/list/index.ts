f6d871f1f2d72993e7395752cee3414f8648c18d
Norbert Kalácska <hello@kalacskanorbert.me>
2020-06-02 10:52:20 +0200

    #15 Added example to nPerPage set in config

--- src/list/index.ts
+++ src/list/index.ts
@@ -1,4 +1,5 @@
 import Main from './main';
+import NPerPage from './nperpage';
 import Pagination from './pagination';
 import NoPagination from './no-pagination';
 import Filters from './filters';
@@ -10,6 +11,7 @@ import Checkbox from './checkbox';
 
 export const list = [
   { label: 'Main', path: '/table', Component: Main },
+  { label: 'N Per Page in Config', path: '/nperpage', Component: NPerPage },
   { label: 'Pagination', path: '/pagination', Component: Pagination },
   { label: 'No Pagination', path: '/no-pagination', Component: NoPagination },
   { label: 'Filtering', path: '/filters', Component: Filters },
