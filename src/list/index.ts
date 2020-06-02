85477212a090f32576e995ef6d24e74d51cd12a0
Norbert Kalácska <hello@kalacskanorbert.me>
2020-06-02 10:39:55 +0200

    Added example for no pagination

--- src/list/index.ts
+++ src/list/index.ts
@@ -1,5 +1,6 @@
 import Main from './main';
 import Pagination from './pagination';
+import NoPagination from './no-pagination';
 import Filters from './filters';
 import Sorting from './sorting';
 import Alert from './alert';
@@ -10,6 +11,7 @@ import Checkbox from './checkbox';
 export const list = [
   { label: 'Main', path: '/table', Component: Main },
   { label: 'Pagination', path: '/pagination', Component: Pagination },
+  { label: 'No Pagination', path: '/no-pagination', Component: NoPagination },
   { label: 'Filtering', path: '/filters', Component: Filters },
   { label: 'Sorting', path: '/sorting', Component: Sorting },
   { label: 'Checks', path: '/checks', Component: Checkbox },
