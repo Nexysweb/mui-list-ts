5b47816e6a714ae489d270d7df8e61c2afc3ff88
johnb8005 <johnb8005@users.noreply.github.com>
2021-05-11 11:30:13 +0000

    deploy: ce37ff617b11649b97e762cb5fa6124de1935519

--- layout/footer.js
+++ layout/footer.js
@@ -1,5 +1,9 @@
+import * as __SNOWPACK_ENV__ from '../_snowpack/env.js';
+
 import React from "../_snowpack/pkg/react.js";
 import {Box, Typography} from "../_snowpack/pkg/@material-ui/core.js";
+const sha = __SNOWPACK_ENV__.SNOWPACK_PUBLIC_GIT_SHA || "git_sha";
+const shaUrl = `https://github.com/Nexysweb/mui-list-ts/commit/${sha}`;
 const Copyright = () => {
   return /* @__PURE__ */ React.createElement(Typography, {
     variant: "body2",
@@ -10,6 +14,8 @@ const Copyright = () => {
 const Footer = () => {
   return /* @__PURE__ */ React.createElement(Box, {
     pt: 4
-  }, /* @__PURE__ */ React.createElement(Copyright, null));
+  }, /* @__PURE__ */ React.createElement(Copyright, null), /* @__PURE__ */ React.createElement("p", null, /* @__PURE__ */ React.createElement("small", null, /* @__PURE__ */ React.createElement("a", {
+    href: shaUrl
+  }, sha))));
 };
 export default Footer;
