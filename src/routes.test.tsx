9d645d29b1bc85f0bce90e7dabea703297593e34
Johan <johan@macair2020>
2021-05-11 15:56:07 +0200

    fix test

--- src/routes.test.tsx
+++ src/routes.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Public from './public';
 
 test('renders learn react link', () => {
