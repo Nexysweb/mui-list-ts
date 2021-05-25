4f40dc3aab3ea5af0dad1618e4c684386ead68d8
John B <john@nexys.ch>
2021-05-25 20:29:53 +0200

    export ui types

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,17 +1,9 @@
 import ListSuper from './list/list-super';
 import PaginationSuper from './list/pagination';
-import * as OrderUtils from './list/utils/order-utils';
-import * as PaginationUtils from './list/utils/pagination-utils';
-import * as FilterUtils from './list/utils/filter-utils';
-import * as Types from './types';
-import * as UITypes from './list/ui-type';
+export * as OrderUtils from './list/utils/order-utils';
+export * as PaginationUtils from './list/utils/pagination-utils';
+export * as FilterUtils from './list/utils/filter-utils';
+export * as Types from './types';
+export * as UITypes from './list/ui-type';
 
-export {
-  PaginationSuper,
-  ListSuper,
-  OrderUtils,
-  PaginationUtils,
-  FilterUtils,
-  Types,
-  UITypes
-};
+export { PaginationSuper, ListSuper };
