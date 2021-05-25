865def1967b8179f6ef6b6f824c7fbf3e6eb2232
johnb8005 <johnb8005@users.noreply.github.com>
2021-05-25 17:15:27 +0000

    deploy: f2e48495685a227b7fda2fd971e6a734dd2345e4

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,4 +1,4 @@
-export const SNOWPACK_PUBLIC_GIT_SHA = "a7a133c6b400883239b9a21899e7f77be8e746e7";
+export const SNOWPACK_PUBLIC_GIT_SHA = "f2e48495685a227b7fda2fd971e6a734dd2345e4";
 export const SNOWPACK_PUBLIC_URL = "/mui-list-ts";
 export const MODE = "production";
 export const NODE_ENV = "production";
